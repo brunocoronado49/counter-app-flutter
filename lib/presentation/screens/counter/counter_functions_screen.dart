4ec1a45e011af4a1d9c00d1624430e6f88590cfe
Bruno Coronado <francorangelcoronado@gmail.com>
2023-05-10 21:17:16 -0600

    Counter app finished

---
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class CounterFunctionsScreen extends StatefulWidget {
+  const CounterFunctionsScreen({super.key});
+
+  @override
+  State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
+}
+
+class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter Functions'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$clickCounter',
+                style:
+                    const TextStyle(fontWeight: FontWeight.w100, fontSize: 160),
+              ),
+              Text(
+                clickCounter == 1 ? "Click" : "Clicks",
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(
+              icon: Icons.refresh_outlined,
+              onPressed: () {
+                setState(() {
+                  if (clickCounter == 0) return;
+                  clickCounter = 0;
+                });
+              },
+            ),
+            const SizedBox(height: 10),
+            CustomButton(
+              icon: Icons.plus_one,
+              onPressed: () {
+                setState(() {
+                  clickCounter++;
+                });
+              },
+            ),
+            const SizedBox(height: 10),
+            CustomButton(
+              icon: Icons.exposure_minus_1_outlined,
+              onPressed: () {
+                setState(() {
+                  if (clickCounter == 0) return;
+                  clickCounter--;
+                });
+              },
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.icon, this.onPressed});
+
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      enableFeedback: true,
+      elevation: 2,
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: const TextStyle(
+                fontWeight: FontWeight.w100,
+                fontSize: 160
+              ),
+            ),
+            Text(
+              clickCounter == 1 ? "Click" : "Clicks",
+              style: const TextStyle(
+                fontSize: 25
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
